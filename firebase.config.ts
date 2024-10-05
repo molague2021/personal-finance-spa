e6956c50ec963f336dfc27b41e01b9679d264046
Martin Olague <molague23@hotmail.com>
2024-10-05 14:57:30 -0600

    added  firebase config

---
+++ firebase.config.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: 'personal-finance-spa.firebaseapp.com',
+  projectId: 'personal-finance-spa',
+  storageBucket: 'personal-finance-spa.appspot.com',
+  messagingSenderId: '560028410503',
+  appId: '1:560028410503:web:bbcd5a4bf8c0c0e260ce22',
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFirestore();
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
 export const App = () => {
+  console.log(process.env.REACT_APP_FIREBASE_API_KEY);
   return <div>Hello, personal finance app!</div>;
 };
